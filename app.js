9e8c0697d0fd5a8ea114b43958f62febadc9c660
sscorp9412 <sharmanischay23@gmail.com>
2020-12-16 12:25:41 +0530

    new update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>This app is created by me you mother fuckers</h1>');
 });
 
 server.listen(port,() => {
