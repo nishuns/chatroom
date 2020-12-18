a6451d8d210b94822d4b8fcbafd4d072c5d102c5
sscorp9412 <sharmanischay23@gmail.com>
2020-12-18 16:36:55 +0530

    uploading my first site

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+var express = require("express");
+var app = express();
+var cors=require('cors');
+var cookieSession=require('cookie-session');
+var bodyParser=require('body-parser');
+var firebase=require('firebase');
+require("firebase/auth");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>This app is created by me you mother fuckers</h1>');
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(cookieSession({
+  name: "my experiment",
+  keys: ["key1", "key2"]
+}))
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDlnVyvwidNZRwoK3LrjIVY8vLuVsgzHWE",
+  authDomain: "scenic-healer-285106.firebaseapp.com",
+  projectId: "scenic-healer-285106",
+  storageBucket: "scenic-healer-285106.appspot.com",
+  messagingSenderId: "174602061790",
+  appId: "1:174602061790:web:241bc1eb076b474815ff4f",
+  measurementId: "G-RG1H6EGQWW"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+app.get("/", function (req, res) {
+    res.render("./pages/main", {
+    });
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+app.get("/takedata", function(req,res){
+  res.send('MY NAME IS NISCHAY');
+})
+
+app.post('/verification', function(req,res){
+  console.log(req.body.ver);
+  res.send('data recieved');
+})
+
+app.get("/success",function(req,res){
+  console.log("successfully registers");
+  res.send('email verified successfully');
+  
+})
+
+
+app.listen(8080);
+console.log("connecting Port :8080\nhttp://localhost:8080");
