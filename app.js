f5297fb4479739411bdd5dc54f0b859720aa670c
sscorp9412 <sharmanischay23@gmail.com>
2020-12-18 17:35:37 +0530

    add new dependencies

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const http = require('http');
+const express=require('express');
 const port = process.env.PORT || 3000
+const app = express()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>This app is created by me you mother fuckers</h1>');
-});
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>This app is created by me you mother fuckers</h1>');
+// });
+
+// server.listen(port,() => {
+//   console.log(`Server running at port `+port);
+// });
