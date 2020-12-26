42386bdfbe95484cc75b311c65a556bb34d28a96
sscorp9412 <sharmanischay23@gmail.com>
2020-12-26 23:22:58 +0530

    msg interface

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const mongoose=require('mongoose');
 const port = process.env.PORT || 3000
 const app = express();
 var http = require('http').createServer(app)
--- public/script/script.js
+++ public/script/script.js
@@ -14,16 +14,23 @@ function removeMessage(){
     request.send();
 }
 
+function getMessage(tune, message){
+    let task=document.createElement('li');
+    task.className=tune;
+    task.innerHTML=message;
+    document.getElementById('list').append(task);
+}
 
 function sendIt(){
     var msg=document.getElementById('task');
+    getMessage('outgoing', msg.value);
+    console.log('Nishu: '+msg.value);
     socket.emit('chat message', msg.value);
     msg.value='';
     return false;
 }
 
 socket.on('chat message', function(msg){
-        let task=document.createElement('li');
-        task.innerHTML=msg;
-        document.getElementById('list').append(task);
+        getMessage('incomming', msg);
+        console.log('child: '+msg);
 });
