0a2d881600b238a64290e9f7fa0fa2a1c2e61bfb
sscorp9412 <sharmanischay23@gmail.com>
2021-01-26 16:09:19 +0530

    Added task API

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const mongoose=require('mongoose');
-const port = process.env.PORT || 3000
 const app = express();
 var http = require('http').createServer(app)
 const io = require('socket.io')(http);
 const bodyParser=require('body-parser');
 require('dotenv');
+const port = process.env.PORT || 4000
 var waiting=null;
 
 mongoose.connect("mongodb+srv://UsersDB:mikkuo8279459923@cluster0.qcost.mongodb.net/UsersDB?retryWrites=true&w=majority", {
@@ -19,10 +19,17 @@ const chatSchema= new mongoose.Schema([{
     name: String,
     message: String
   }]
-}])
+}]);
+
+const taskSchema=new mongoose.Schema([{
+  message: String,
+  read: Boolean
+}]);
+
 mongoose.set("useCreateIndex", true);
 
 const Chat= new mongoose.model('Chat', chatSchema);
+const Task=new mongoose.model('Task', taskSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -100,6 +107,34 @@ io.on('connection', (socket) => {
 
   });
 });
+
+app.get('/api/tasks', (req,res)=>{
+  Task.find({}, (err,tasks)=>{
+    if(err) console.log(err);
+    res.send({tasks: tasks});
+  })
+});
+
+app.get('/api/task/:taskid', (req,res)=>{
+  Task.findOne({_id: req.params.taskid}, (err, task)=>{
+    if(err) console.log(err);
+    res.send(task);
+  })
+})
+
+app.post('/api/tasks/:task', (req,res)=>{
+  let task=new Task({
+    message: req.params.task,
+    read: false
+  });
+
+  task.save(function(err,data){
+    if(err) console.log(err)
+    else{
+      res.send("Task Added successfully");
+    }
+  })
+})
 
 http.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
